ef144ccea181ea67f45bd860f1e00776a9fa6c9a
Kurt Lopez <kurtdevph@gmail.com>
2019-11-14 07:06:07 +0800

    Updates

---
+++ precache-manifest.2074d9f884dfac77e7f855d961857284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc3aa0e74d1055c983bd379bc90f596",
+    "url": "/robolist/index.html"
+  },
+  {
+    "revision": "66e39f27f63dc3fa7b61",
+    "url": "/robolist/static/css/main.1d4d5ee6.chunk.css"
+  },
+  {
+    "revision": "5a1f89e74b4df36cd542",
+    "url": "/robolist/static/js/2.dc253644.chunk.js"
+  },
+  {
+    "revision": "66e39f27f63dc3fa7b61",
+    "url": "/robolist/static/js/main.a842c8fc.chunk.js"
+  },
+  {
+    "revision": "03c1adcc682ebc00213d",
+    "url": "/robolist/static/js/runtime-main.a7397b63.js"
+  }
+]);
